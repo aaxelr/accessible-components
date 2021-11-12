688ab3cb50d53a04b7a2074859e683eb2783953d
aaxelr <antonio.axelsson@gmail.com>
2021-11-12 20:32:33 +0100

    add routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import MaterialPage from './pages/MaterialPage';
+import ChakraPage from './pages/ChakraPage';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route exact path="/" element={<LandingPage />} />
+      <Route path="/material" element={<MaterialPage />} />
+      <Route path="/chakra" element={<ChakraPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
 
