540f8cb5faebdace28863c1649c45829202ca2e7
aaxelr <antonio.axelsson@gmail.com>
2021-11-16 12:22:24 +0100

    create MUI contact form component

---
+++ src/components/MUIContactForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import { Typography } from '@mui/material';
+
+function MUIContactForm() {
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '1rem',
+      }}
+    >
+      <Typography variant="h5" component="h3">
+        Contact
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="contact-form-name-input"
+          label="Name"
+          type="text"
+          placeholder="Your Name"
+          maxRows={4}
+        />
+        <TextField
+          id="contact-form-email-input"
+          label="Email"
+          placeholder="em@il.com"
+          multiline
+        />
+        <TextField
+          id="contact-form-comment-input"
+          label="Comment"
+          placeholder="Your comment here..."
+          multiline
+          rows={4}
+          fullWidth
+        />
+      </Box>
+    </Container>
+  );
+}
+
+export default MUIContactForm;
