9a7fc5005445f441af853b1f91e61c01d4c662d2
aaxelr <antonio.axelsson@gmail.com>
2021-11-16 12:22:53 +0100

    add MUI contact form to material page

--- src/pages/MaterialPage.jsx
+++ src/pages/MaterialPage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Container from '@mui/material/Container';
 import MUIArticles from '../components/MUIArticles';
+import MUIContactForm from '../components/MUIContactForm';
 import MUIHeader from '../components/MUIHeader';
 
 function MaterialPage() {
   return (
     <React.Fragment>
       <MUIHeader />
-      <MUIArticles />
-      {/* CONTACT FORM */}
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          margin: '1rem',
+        }}
+      >
+        <MUIArticles />
+        <MUIContactForm />
+      </Container>
       {/* FOOTER */}
     </React.Fragment>
   );
