5cef0cdb10ff13ecc7125506325ab0e70e1f507d
aaxelr <antonio.axelsson@gmail.com>
2021-11-18 15:27:40 +0100

    create ChakraHeader

---
+++ src/components/ChakraHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box, Flex, Heading, Spacer } from '@chakra-ui/layout';
+import ChakraLogin from './ChakraLogin';
+
+function ChakraHeader() {
+  return (
+    <Flex bg="blue.500" color="white">
+      <Box p="3">
+        <Heading as="h1" size="2xl">Chakra UI</Heading>
+      </Box>
+      <Spacer />
+      <ChakraLogin />
+    </Flex>
+  );
+}
+
+export default ChakraHeader;
