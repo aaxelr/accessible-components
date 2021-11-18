42c17af52acdd5c80b1773c324e2587383329fa3
aaxelr <antonio.axelsson@gmail.com>
2021-11-18 15:34:27 +0100

    create ChakraLogin

---
+++ src/components/ChakraLogin.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Center } from '@chakra-ui/layout';
+import {
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react"
+import ChakraModal from './ChakraModal';
+
+function ChakraLogin() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <React.Fragment>
+      <Center m="3">
+        <Button colorScheme="pink" onClick={onOpen}>Log in</Button>
+      </Center>
+      <ChakraModal 
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </React.Fragment>
+  );
+}
+
+export default ChakraLogin;
