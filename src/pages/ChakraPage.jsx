0a05a50e09c38310b0c089a21077638eb4cf4ee8
aaxelr <antonio.axelsson@gmail.com>
2021-11-19 14:23:07 +0100

    update imports

--- src/pages/ChakraPage.jsx
+++ src/pages/ChakraPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react"
-import ChakraHeader from '../components/ChakraHeader';
-import ChakraArticles from '../components/ChakraArticles';
-import ChakraContactForm from '../components/ChakraContactForm';
-import ChakraFooter from '../components/ChakraFooter';
+import ChakraHeader from '../components/Chakra/ChakraHeader';
+import ChakraArticles from '../components/Chakra/ChakraArticles';
+import ChakraContactForm from '../components/Chakra/ChakraContactForm';
+import ChakraFooter from '../components/Chakra/ChakraFooter';
 
 function ChakraPage() {
   return (
