1f982e3603bef25e68775898c83f653b49e078ec
aaxelr <antonio.axelsson@gmail.com>
2021-11-19 13:36:26 +0100

    add contact form to ChakraPage

--- src/pages/ChakraPage.jsx
+++ src/pages/ChakraPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react"
 import ChakraHeader from '../components/ChakraHeader';
 import ChakraArticles from '../components/ChakraArticles';
+import ChakraContactForm from '../components/ChakraContactForm';
 
 function ChakraPage() {
   return (
     <ChakraProvider>
       <ChakraHeader />
       <ChakraArticles />
+      <ChakraContactForm />
     </ChakraProvider>
   );
 }
